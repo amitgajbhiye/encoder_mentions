c72e57482792506895919dd6c310726737f60b6e
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-08 15:48:40 +0100

    mention enc

---
+++ run_mention_encoder_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=tMenEnc
+
+#SBATCH --output=logs/mention_enc/out_dummy_train_model.txt
+#SBATCH --error=logs/mention_enc/err_dummy_train_model.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100,gpu
+#SBATCH --gres=gpu:3
+
+#SBATCH --mem=16G
+#SBATCH -t 0-05:00:00
+
+conda activate venv
+
+python3 src/model.py --config_file configs/mention/bert_large.json
+
+echo 'Job Finished !!!'
