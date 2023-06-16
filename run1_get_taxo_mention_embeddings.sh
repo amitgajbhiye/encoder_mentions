db0b864252602e415a35278a62d6602d50f3aea8
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-17 00:12:12 +0100

    con sent embed

---
+++ run1_get_taxo_mention_embeddings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=getMenEmb_all_taxonomy
+
+#SBATCH --output=logs/get_mention_embeddings/out_get_taxonomy_mention_embeddings.txt
+#SBATCH --error=logs/get_mention_embeddings/err_get_taxonomy_mention_embeddings.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=30G
+#SBATCH -t 1-00:00:00
+
+conda activate venv
+
+python3 src/get_mention_embeddings.py --config_file configs/get_mention_embeddings/environment_ev_taxo.json
+python3 src/get_mention_embeddings.py --config_file configs/get_mention_embeddings/equipment_taxo.json
+python3 src/get_mention_embeddings.py --config_file configs/get_mention_embeddings/food_taxo.json
+python3 src/get_mention_embeddings.py --config_file configs/get_mention_embeddings/science_taxo.json
+
+echo 'Job Finished !!!'
