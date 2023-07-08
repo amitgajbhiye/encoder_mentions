a22f33a8c1f0132a4ce011efb983475a447ea616
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-07-08 10:57:23 +0100

    get definition embeddings

--- download_models.sh
+++ download_models.sh
@@ -8,5 +8,8 @@ cd trained_models
 
 echo "Downloading Pretrained Models ..."
 
-wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_definition_encoder_wordnet_codwoe.pt
+#BERT-large mention encoder
 wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_mention_encoder_cnetpchatgpt_5k_cons.pt
+
+## BERT-large Definition Encoder
+wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_definition_encoder_wordnet_codwoe.pt
