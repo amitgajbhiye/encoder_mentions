e7e4a798b93c4ad282330c93ddf159b101fa0ccb
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-08-16 11:12:33 +0100

    propertycontra coda 21 eval

--- src/coda_evaluation.py
+++ src/coda_evaluation.py
@@ -162,16 +162,15 @@ random.seed(42)
 
 # emb_model = SentenceTransformer(sentence_model, device=f"cuda:{gpu_id}")
 
-sentence_model = "conceptcontra_bert_large_uncased"
+sentence_model = "propertycontra_bert_large_uncased"
 
 data_folder = "data/coda21"
 output_folder = f"output/{sentence_model}"
 os.makedirs(output_folder, exist_ok=True)
 
 # config_file = "configs/coda21/coda21.json"
-config_file = "configs/coda21/conceptcontra.json"
-
-# config_file = "configs/coda21/propertycontra.json"
+# config_file = "configs/coda21/conceptcontra.json"
+config_file = "configs/coda21/propertycontra.json"
 
 print(f"**** config_file ****: {config_file}", flush=True)
 
