4ff8e1fcfc168cf1ce75baf1e6601ab60f73f1b4
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-09-13 14:00:04 +0100

    new models update

--- download_models.sh
+++ download_models.sh
@@ -5,11 +5,11 @@ mkdir -p trained_models
 
 cd trained_models
 
-
 echo "Downloading Pretrained Models ..."
 
 #BERT-large mention encoder
 wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_mention_encoder_cnetpchatgpt_5k_cons.pt
 
+
 ## BERT-large Definition Encoder
-wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_definition_encoder_wordnet_codwoe.pt
+wget https://huggingface.co/amitgajbhiye/mention_and_definition_encoders/resolve/main/bert_large_definition_encoder_wordnet_codwoe.pt.pt
