abde63d6a82e97f1e8d849bcf9f4d1f3785f368b
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-10-03 17:13:35 +0100

    bienc intrinsic eval

---
+++ run14_bienc_intrinsic_eval.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=BiEncInt
+
+#SBATCH --output=logs/bienc_intrinsic_eval/out_bert_large.txt
+#SBATCH --error=logs/bienc_intrinsic_eval/err_bert_large.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=20G
+#SBATCH -t 0-01:00:00
+
+conda activate venv
+
+python3 src/bienc_intrinsic_eval.py --config configs/bienc_intrinsic_eval/bert_large.json
+
+echo 'Job Finished !!!'
