6d070f518f3b7fd43b62a0dbb9876bd43a90969a
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-10-11 11:58:37 +0100

    mcrae concept mention embeds all setups

---
+++ run7_setup4_mention_embeds_mcrae_concepts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=setup4_mention_embeds_mcrae_concepts
+
+#SBATCH --output=logs/get_mention_embeddings/out_setup4_mention_embeds_mcrae_concepts.txt
+#SBATCH --error=logs/get_mention_embeddings/err_setup4_mention_embeds_mcrae_concepts.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=16G
+#SBATCH -t 0-02:00:00
+
+conda activate venv
+
+python3 src/get_mention_embeddings.py --config_file configs/get_mention_embeddings/setup4_mention_embeds_mcrae_concepts.json
+
+echo 'Job Finished !!!'
