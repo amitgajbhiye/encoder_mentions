755764143ad36dd896203043629e61595702a3bf
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-10-20 19:13:11 +0100

    with con embed ccdex dex mcrae prop split eval

---
+++ run25_1knn_dex_withcon_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=WCDexKNN_BiEncInt
+
+#SBATCH --output=logs/bienc_intrinsic_eval/out_1knn_dex_withcon_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.txt
+#SBATCH --error=logs/bienc_intrinsic_eval/err_1knn_dex_withcon_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=20G
+#SBATCH -t 2-00:00:00
+
+conda activate venv
+
+python3 src/knn_biencoder_intrinsic_eval.py --config configs/bienc_intrinsic_eval/1knn_dex_withcon_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.json
+
+echo 'Job Finished !!!'
