5f9a7a4bb7440d651984294b796d204d149833a6
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-10-20 21:17:25 +0100

    with con embed ccdex dex mcrae prop split eval

--- run23_1knns_dex_without_con_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.sh
+++ run23_1knns_dex_without_con_embeds_mcrae_setup1_bert_large_bienc_intrinsic_eval.sh
@@ -9,11 +9,11 @@
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p gpu_v100
+#SBATCH -p gpu,gpu_v100
 #SBATCH --gres=gpu:1
 
 #SBATCH --mem=20G
-#SBATCH -t 2-00:00:00
+#SBATCH -t 1-00:00:00
 
 conda activate venv
 
